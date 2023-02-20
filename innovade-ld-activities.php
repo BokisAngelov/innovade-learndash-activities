5ea16ed29b8a119efb54de0ef0936ddef6cc29d7
BokisAngelov <bokisangelov@gmail.com>
2023-02-20 10:49:08 +0200

    Add files via upload

---
+++ innovade-ld-activities.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Plugin Name: Innovade Learndash Activities
+ * Description: 
+ * Version: 1.0
+ * Requires at least: 4.8
+ * Tested up to: 6.1
+ * Author: Innovade LI
+ * Author URI: https://innovade.eu
+ * Text Domain: innovade-learndash-activites
+ */
+
